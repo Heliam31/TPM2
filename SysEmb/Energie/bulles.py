f91f1fee056ac94efee27644d1b7f2ff22f1fcb4
Heliam <mihaipoitelea11@gmail.com>
2023-11-22 11:56:30 +0100

    Gridaj

---
+++ SysEmb/Energie/bulles.py
@@ -0,0 +1,10 @@
+def triBulles(L):
+    for i in range(len(L),0,-1):
+        for j in range(0,i-1):
+            if L[j+1] < L[j]:
+                L[j+1] , L[j] = L[j] , L[j+1]
+                
+    return L
+
+L=[12,82,42,71,238,72,1,465,23,53]
+triBulles(L)
---
+++ SysEmb/Energie/test.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def _read_csv(filename):
+    df = pd.read_csv(filename, sep=' ', skipinitialspace=True)
+    if df.columns[-1].startswith('Unnamed'):
+        df.drop(columns=df.columns[-1:], axis=1, inplace=True)
+    return df
+
+def show_csv(filename, norm=False):
+    a = _read_csv(filename)
+    a['#timestamp'] = a['#timestamp']-a['#timestamp'][0]
+    if norm:
+        tmp = (a/a.max())
+        tmp['#timestamp'] = a['#timestamp']
+        a = tmp
+    a.plot(x='#timestamp', title =filename)
+
+show_csv('/home/heliam/TPM2/SysEmb/Energie/dataSel.csv', norm=False)
+show_csv('/home/heliam/TPM2/SysEmb/Energie/dataBul.csv', norm=False)
+plt.show(block=True)
